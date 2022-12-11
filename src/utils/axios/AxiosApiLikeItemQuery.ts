24d918c5a683b46a16798db7f93925a63f634a4b
LAPTOP-1FO0QCOS\park9 <park970320@gmail.com>
2022-12-11 16:09:35 +0900

    Add: api 호출식이 쿼리에 따라 더 필요할 것 같아서 추가

---
+++ src/utils/axios/AxiosApiLikeItemQuery.ts
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const AxiosApiLikeItemQuery = async(word:string, date:string) => {
+  const apiData = await axios.get(
+    `https://apis.data.go.kr/1160100/service/GetStockSecuritiesInfoService/getStockPriceInfo?serviceKey=g4VOdRGXDRyXBvs%2FYEYpLGMnX%2BOQE1wfNEJipSDMlweYUrYOwCO8hdh261HHPkFDbvSkVyRE388W0q84%2BfPZHw%3D%3D&numOfRows=10&pageNo=1&resultType=json&basDt=${date}&likeItmsNm=${word}`,
+    {
+      headers:{
+        "Content-Type": "application/json"
+      }
+    }
+  )
+  return apiData
+};
+
+export default AxiosApiLikeItemQuery;
