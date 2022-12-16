4a421f118dca904fb6bddfe19601580cdc507010
LAPTOP-1FO0QCOS\park9 <park970320@gmail.com>
2022-12-16 20:07:49 +0900

    style: list 상세보기 작업중

--- src/service/axios/AxiosApi.ts
+++ src/service/axios/AxiosApi.ts
@@ -1,25 +1,39 @@
 import axios from 'axios';
-
+const customAxios = axios.create({
+  baseURL: 'https://apis.data.go.kr/1160100/service/GetStockSecuritiesInfoService',
+  timeout: 1000,
+  headers:{
+    "Content-Type": "application/json"
+  },
+})
 const AxiosApiItemQuery = async(word:string, date:string) => {
-  const apiData = await axios.get(
-    `https://apis.data.go.kr/1160100/service/GetStockSecuritiesInfoService/getStockPriceInfo?serviceKey=g4VOdRGXDRyXBvs%2FYEYpLGMnX%2BOQE1wfNEJipSDMlweYUrYOwCO8hdh261HHPkFDbvSkVyRE388W0q84%2BfPZHw%3D%3D&numOfRows=10&pageNo=1&resultType=json&basDt=${date}&itmsNm=${word}`,
-    {
-      headers:{
-        "Content-Type": "application/json"
-      }
-    }
+  const apiData = await customAxios.get('getStockPriceInfo',{
+    params: {
+      serviceKey: 'g4VOdRGXDRyXBvs%2FYEYpLGMnX%2BOQE1wfNEJipSDMlweYUrYOwCO8hdh261HHPkFDbvSkVyRE388W0q84%2BfPZHw%3D%3D',
+      numOfRows: '10',
+      pageNo: '1',
+      resultType: 'json',
+      basDt: `${date}`,
+      itmsNm:`${word}`,
+    },
+  }
+    // `?serviceKey=g4VOdRGXDRyXBvs%2FYEYpLGMnX%2BOQE1wfNEJipSDMlweYUrYOwCO8hdh261HHPkFDbvSkVyRE388W0q84%2BfPZHw%3D%3D&numOfRows=10&pageNo=1&resultType=json&basDt=${date}&itmsNm=${word}`,
   )
   return apiData
 };
 
 const AxiosApiLikeItemQuery = async(word:string, date:string) => {
-  const apiData = await axios.get(
-    `https://apis.data.go.kr/1160100/service/GetStockSecuritiesInfoService/getStockPriceInfo?serviceKey=g4VOdRGXDRyXBvs%2FYEYpLGMnX%2BOQE1wfNEJipSDMlweYUrYOwCO8hdh261HHPkFDbvSkVyRE388W0q84%2BfPZHw%3D%3D&numOfRows=10&pageNo=1&resultType=json&basDt=${date}&likeItmsNm=${word}`,
-    {
-      headers:{
-        "Content-Type": "application/json"
-      }
-    }
+  const apiData = await customAxios.get('getStockPriceInfo',{
+    params: {
+      serviceKey: 'g4VOdRGXDRyXBvs%2FYEYpLGMnX%2BOQE1wfNEJipSDMlweYUrYOwCO8hdh261HHPkFDbvSkVyRE388W0q84%2BfPZHw%3D%3D',
+      numOfRows: '10',
+      pageNo: '1',
+      resultType: 'json',
+      basDt: `${date}`,
+      likeItmsNm:`${word}`,
+    },
+  }
+    // `?serviceKey=g4VOdRGXDRyXBvs%2FYEYpLGMnX%2BOQE1wfNEJipSDMlweYUrYOwCO8hdh261HHPkFDbvSkVyRE388W0q84%2BfPZHw%3D%3D&numOfRows=10&pageNo=1&resultType=json&basDt=${date}&likeItmsNm=${word}`,
   )
   return apiData
 };
